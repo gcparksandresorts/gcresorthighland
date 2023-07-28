77f716013fc30fb26d10fcd9f490b29347b6f30a
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-07-27 18:50:03 -0700

    Update headerMain.js

--- webresources/scripts/headerMain.js
+++ webresources/scripts/headerMain.js
@@ -3,6 +3,8 @@
 	const footBar = document.getElementById("footer");
 
 	function runHeaderSetup(){
+		Header.innerHTML = "<div id='headerBar'></div>";
+		createFooter();
 		initializeHeader();
 	}
 
@@ -12,7 +14,7 @@
 	window.onresize = initializeHeader();
 
 	function initializeHeader(){
-		Header.innerHTML = "<div id='headerBar'></div>";
+		
 
 		const headBar = document.getElementById("headerBar");
 		headBar.innerHTML = "<img id='headerBarLogo' src='https://gcparksandresorts.github.io/gcresorthighland/webresources/images/logos/gcResortLogoRemade.png' onclick='funcGoToHome();'>";
@@ -20,6 +22,25 @@
 													<button class='headerBarButton'>Special Events</button> \
 													<button class='headerBarButton'>Hotels and Resorts</button>  \
 													<button class='headerBarButton'>Things to Do &#9661;</button>";
+	}
+
+//---------------------------------------------------------------------------------------------------------------------------------- FOOTER SETUP
+	function createFooter(){
+		footBar.innerHTML = "<div class='spacer'></div>";
+
+		footBar.innerHTML += "<div class='spacer'></div> \
+				     <div class='container' style='text-align:center;line-height:2;'> \
+				     <b>Get in Touch</b> <br> (312) 988 1000 <br> <a>guestservices.gcrh@gcpr.net</a> </div>";
+		
+		footBar.innerHTML += "<div class='spacer'></div> \
+			              <div style='text-align:center'> <br> \
+			              <img src='https://gcparksandresorts.github.io/gcresorthighland/webresources/images/logos/gcprGrayLogo.png' style='width:300px;height:auto;margin:auto;'> \
+			              </div> <br>";
+		
+		footBar.innerHTML += "<div style='text-align:center'> <p class='disclaimer'>©2023 Globe-City Parks and Resorts LLC.<br></p> \
+    				      <a>Privacy Policy</a> | <a>Legal Information</a> | <a>MCBPO</a> | <a>Careers</a> \
+    				      <br><br>    </div>";
+		
 	}
 
 
