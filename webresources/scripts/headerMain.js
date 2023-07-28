bf0b89dac68945c80160241d98628a2159fb74d5
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-07-27 17:09:43 -0700

    Update headerMain.js

--- webresources/scripts/headerMain.js
+++ webresources/scripts/headerMain.js
@@ -1,1 +1,29 @@
+//---------------------------------------------------------------------------------------------------------------------------------- VARIABLES AND INITIALIZATION
+  const headBar = document.getElementById("header");
+	const footBar = document.getElementById("footer");
+
+	function runHeaderSetup(){
+		initializeHeader();
+	}
+
+	
+
+//---------------------------------------------------------------------------------------------------------------------------------- HEADER SETUP
+	window.onresize = initializeHeader();
+
+	function initializeHeader(){
+		headBar.innerHTML = "<img id='headerBarLogo' src='https://gcparksandresorts.github.io/gcresorthighland/webresources/images/logos/gcResortLogoRemade.png' onclick='funcGoToHome();'>";
+		headBar.innerHTML += "<button id='headerBarTP'>Tickets and Passes</button>  <button class='headerBarButton'>More &#9661;</button>  \
+													<button class='headerBarButton'>Special Events</button> \
+													<button class='headerBarButton'>Hotels and Resorts</button>  \
+													<button class='headerBarButton'>Things to Do &#9661;</button>";
+	}
+
+
+//---------------------------------------------------------------------------------------------------------------------------------- PAGE LOADING FUNCTIONS 
+  function funcGoToHome(){window.open("https://gcparksandresorts.github.io/gcresorthighland","_self");}
+  
+  function endLoadingInit(){
+  	document.getElementById('loader').style.display = 'none';
+  }
 
