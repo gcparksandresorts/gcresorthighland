6bf2262fdfc29eb6ec28164f2632633f9a71e04a
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-07-27 23:00:26 -0700

    Update resortSpotlight.js

--- webresources/scripts/data/resortSpotlight.js
+++ webresources/scripts/data/resortSpotlight.js
@@ -10,16 +10,16 @@
 // ------------------------------------------------------------------------------------------------------------------------------- SPOTLIGHT CONTENT
 const january = [];
       january[0] = "https://dummyimage.com/640x360/fff/aaa";
-      january[1] = "<h3>Season of Light Continues</h3><p>Globe-City's Season of Light<sup>TM</sup> celebration isn't over yet, Visit before time runs out!";
+      january[1] = "<h3>Season of Light Continues</h3>Globe-City's Season of Light<sup>TM</sup> celebration isn't over yet, Visit before time runs out!";
       january[2] = "https://www.youtube.com/watch?v=ja1sXvNCyO0&ab_channel=FunnyCatsLife";
 
-      january[0] = "https://dummyimage.com/640x360/fff/aaa";
-      january[1] = "<h3>Season of Light Continues</h3><p>Globe-City's Season of Light<sup>TM</sup> celebration isn't over yet, Visit before time runs out!";
-      january[2] = "https://www.youtube.com/watch?v=ja1sXvNCyO0&ab_channel=FunnyCatsLife";
+      january[3] = "https://dummyimage.com/640x360/fff/aaa";
+      january[4] = "<h3>Season Passes on Sale</h3>As the holiday season wraps up, now is a good time to buy an annual pass and extend your adventure";
+      january[5] = "https://www.youtube.com/watch?v=ja1sXvNCyO0&ab_channel=FunnyCatsLife";
 
-      january[0] = "https://dummyimage.com/640x360/fff/aaa";
-      january[1] = "<h3>Season of Light Continues</h3><p>Globe-City's Season of Light<sup>TM</sup> celebration isn't over yet, Visit before time runs out!";
-      january[2] = "https://www.youtube.com/watch?v=ja1sXvNCyO0&ab_channel=FunnyCatsLife";
+      january[6] = "https://dummyimage.com/640x360/fff/aaa";
+      january[7] = "<h3>Happy New Year!</h3>From all of us here at the resort, may this year be full of fun and new adventures <3";
+      january[8] = "https://www.youtube.com/watch?v=ja1sXvNCyO0&ab_channel=FunnyCatsLife";
 
 // ------------------------------------------------------------------------------------------------------------------------------- MONTHS
 const monthsNamed = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
