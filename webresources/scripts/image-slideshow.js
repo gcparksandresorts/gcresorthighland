76f885cbf35ce0198d4cf38cf18f079549221b1e
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-08-08 14:40:51 -0700

    Create image-slideshow.js

---
+++ webresources/scripts/image-slideshow.js
@@ -0,0 +1,48 @@
+//Label Image ID's with the following: im01, im02, im03, im04
+
+const im1 = document.getElementById("im01");
+const im2 = document.getElementById("im02");
+const im3 = document.getElementById("im03");
+const im4 = document.getElementById("im04");
+
+
+function runSlideShowFour(s){
+  slideTime = s;
+  
+  im1.style.animation = "FadeIn 0.40s";
+  im2.style.animation = "FadeIn 0.40s";
+  im3.style.animation = "FadeIn 0.40s";
+  im4.style.animation = "FadeIn 0.40s";
+
+  im1.style.display = "block";
+  im2.style.display = "none";
+  im3.style.display = "none";
+  im4.style.display = "none";
+
+  setInterval(nextSlide(2), slideTime);
+}
+
+function nextSlide(n){
+  switch(n){
+    case 1:
+      im1.style.display = "block";
+      im4.style.display = "none";
+      setInterval(nextSlide(2), slideTime);
+      break;
+    case 2:
+      im1.style.display = "none";
+      im2.style.display = "block";
+      setInterval(nextSlide(3), slideTime);
+      break;
+    case 3:
+      im2.style.display = "none";
+      im3.style.display = "block";
+      setInterval(nextSlide(4), slideTime);
+      break;
+    case 4:
+      im3.style.display = "none";
+      im4.style.display = "block";
+      setInterval(nextSlide(1), slideTime);
+      break;
+  }
+}
