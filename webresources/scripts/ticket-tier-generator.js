d1e84d6ec92c430f0e0a684bd5f4b21b37724aae
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-08-09 20:15:17 -0700

    Create ticket-tier-generator.js

---
+++ webresources/scripts/ticket-tier-generator.js
@@ -0,0 +1,10 @@
+const tier = document.getElementById("input1");
+const entertainment = document.getElementById("input2");
+const events = document.getElementById("input3");
+const closures = document.getElementById("input4");
+
+const dateDisplay = document.getElementById("dateDisplayText");
+const finalDisplay = document.getElementById("finalDisplayText");
+
+finalVariable = 0;
+
