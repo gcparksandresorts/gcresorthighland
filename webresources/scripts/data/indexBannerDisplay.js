51682a88cea7344565ad0187b5e4a39675d45380
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-08-18 10:37:36 -0700

    Update indexBannerDisplay.js

--- webresources/scripts/data/indexBannerDisplay.js
+++ webresources/scripts/data/indexBannerDisplay.js
@@ -8,19 +8,22 @@
 const bannerTxtDisp = [];
 
   //General Use
-  bannerTxtDisp[0] = "<h2>It's time to embark on a new Adventure!</h2>Travel new roads and explore mystical worlds here at the Globe-City Resort Highland!<br><a href='#ttdSection'>Learn More</a>";
+  bannerTxtDisp[0] = "<h2>It's time to embark on a new Adventure</h2>Travel new roads and explore mystical worlds here at the Globe-City Resort Highland!<br><a href='#ttdSection'>Learn More</a>";
 
   // SummerFest
-  bannerTxtDisp[1] = "<h2>Globe-City's Summer Fest<sup>TM</sup> is Here!</h2>Gather your family and friends and set forth on an adventure of a lifetime! \
+  bannerTxtDisp[1] = "<h2>Globe-City's Summer Fest<sup>TM</sup> is Here</h2>Gather your family and friends and set forth on an adventure of a lifetime! \
                       Partake in the summer festivities and let your spirits soar as you create memories to cherish forever!  \
                       <br><a href='https://gcparksandresorts.github.io/gcresorthighland/events/summerfest'>View Special Event</a>";
   
   // Season of Light
-  bannerTxtDisp[2] = "<h2>The Holidays Are Here!</h2> Season of Light<sup>TM</sup> has returned! Immerse yourself in the holiday spirit, from enchanting light displays to heartwarming live performances. Bring your loved ones and create cherished memories as you share laughter, joy, and the true essence of the holidays. \
+  bannerTxtDisp[2] = "<h2>The Holidays Are Here</h2> Season of Light<sup>TM</sup> has returned! Immerse yourself in the holiday spirit, from enchanting light displays to heartwarming live performances. Bring your loved ones and create cherished memories as you share laughter, joy, and the true essence of the holidays. \
                       <br><a href='https://gcparksandresorts.github.io/gcresorthighland/events/season-of-light' >View Special Event</a>";
 
   // Spooky Season
   bannerTxtDisp[3] = "<h2>The Spookiest Season of All</h2>This Halloween, experience new frights, sights, and delights with the return of Sally's Halloween After-Party<sup>TM</sup>!<br><a href='https://gcparksandresorts.github.io/gcresorthighland/events/sallys-halloween-party' >View Special Event</a>";
+
+  // New Years
+  bannerTxtDisp[4] = "<h2>Welcome to the New Year</h2>This year, set fourth on new Adventures and create memories to cherish forever, only at the Globe-City Resort Highland!<br><a href='https://gcparksandresorts.github.io/gcresorthighland/tickets'>Buy Tickets</a>";
 
 
 // ----------------------------------------------------------------------------------------------------------------------------- BANNER DISPLAYS
@@ -41,8 +44,8 @@ function setBannerDisplays(){
   switch(todayMonth){
       
     case 0:                               // January
-      vidBannerTxtBox.innerHTML =  bannerTxtDisp[2];
-      videoBanner.innerHTML = "<img id='videoBanner' src='https://i.pinimg.com/originals/da/d6/33/dad6333674d70d671f80fa4a3d2a784f.gif'>";
+      vidBannerTxtBox.innerHTML =  bannerTxtDisp[4];
+      videoBanner.innerHTML = "<img id='videoBanner' src='https://www.disneyholidays.co.uk/disneyland-paris/images/planning/mobile/new-years-eve-fireworks-hero-sleeping-beauty-castle.jpg'>";
       break;
     case 10:                              // November
       vidBannerTxtBox.innerHTML =  bannerTxtDisp[2];
