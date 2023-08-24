28cd697517526e3417071fdd5b4310c51a3cb625
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-08-24 09:15:02 -0700

    Update daytier.js

--- webresources/scripts/data/daytier.js
+++ webresources/scripts/data/daytier.js
@@ -1,1 +1,44 @@
+// ------------------------------------------------------------------------------ PARK HOURS  ---------------------------------------------------------------------------------------------------
+parkHoursList = []; // Adventure Park : CityShop : Special Event
+
+parkHoursList[0] = "Error : Error";
+parkHoursList[1] = "10AM - 8PM : 11AM - 10PM";
+parkHoursList[2] = "10AM - 9PM : 11AM - 11PM";
+parkHoursList[3] = "10AM - 10PM : 11AM - 11PM";
+parkHoursList[4] = "9AM - 11PM : 11AM - 12AM";
+parkHoursList[5] = "9AM - 11PM : 11AM - 12AM";
+parkHoursList[6] = "10AM - 8PM : 11AM - 10PM : 6PM - 11PM"; // Special Event
+parkHoursList[7] = "10AM - 8PM : 11AM - 10PM : 7PM - 1AM"; // Special Event
+
+
+// ------------------------------------------------------------------------------ ENTERTAINMENT -------------------------------------------------------------------------------------------------
+entertainmentList = []; // Name | Hours | Image_Link | Click_Link ||  Name | Hours | Image_Link | Click_Link || ...
+
+entertainmentList[0] = "No Shows Today";
+entertainmentList[1] = "Globe-City Marching Band | 11:00AM, 12:00PM, 2:00PM, 3:00PM | https://brettalan.com/wp-content/uploads/2019/01/00004-Grossmont-District-Showcase-2018-D7A_5349.jpg | https://gcparksandresorts.github.io/gcresorthighland/destinations/adventure-park/entertainment/gc-marching-band";
+entertainmentList[2] = "";
+entertainmentList[3] = "";
+entertainmentList[4] = "";
+
+
+// ------------------------------------------------------------------------------ SPECIAL EVENTS ------------------------------------------------------------------------------------------------
+eventList = []; // Name | Hours (or 'special event') | Image_Link | Click_Link ||  Name | Hours (or 'special event') | Image_Link | Click_Link || ...
+
+eventList[0] = "No Events Today";
+eventList[1] = "No Events Today";
+eventList[2] = "No Events Today";
+eventList[3] = "No Events Today";
+
+
+dateTierList = [];
+// ------------------------------------------------------------------------------ PASTE BELOW THIS LINE -----------------------------------------------------------------------------------------
+dateTierList[0] = "January 1 - Monday - 1 - 1 - 1"; // Monday, January 1st
+dateTierList[1] = "January 2 - Tuesday - 1 - 1 - 1"; // Tuesday, January 2st
+dateTierList[2] = "January 3 - Wednesday - 1 - 1 - 1"; // Wednesday, January 3st
+dateTierList[3] = "January 4 - Thursday - 1 - 1 - 1"; // Thursday, January 4st
+dateTierList[4] = "August 24 - Thursday - 1 - 1 - 1"; // Thursday, January 4st
+dateTierList[5] = "August 25 - Friday - 1 - 1 - 1"; // Thursday, January 4st
+
+
+
 
