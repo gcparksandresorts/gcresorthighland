60011dc326b4820cc069412ddf311080f0cce1ad
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-09-11 10:01:02 -0700

    Update daytier.js

--- webresources/scripts/data/daytier.js
+++ webresources/scripts/data/daytier.js
@@ -11,26 +11,59 @@ parkHoursList[6] = "10:00 AM - 8:00 PM | 11:00 AM - 10:00 PM | 6:00 PM - 11:00 P
 parkHoursList[7] = "10:00 AM - 8:00 PM | 11:00 AM - 10:00 PM | 7:00 PM - 12:00 AM"; // Special Event
 
 
+
 // ------------------------------------------------------------------------------ ENTERTAINMENT -------------------------------------------------------------------------------------------------
 entertainmentList = []; // Name | Hours | Image_Link | Click_Link ||  Name | Hours | Image_Link | Click_Link || ...
+// entertainmentList[N] = "Name | Hours | Image_link | click_link";
 
 entertainmentList[0] = "No Shows Today";
-entertainmentList[1] = "Globe-City Marching Band | 11:00AM, 12:00PM, 2:00PM, 3:00PM | https://brettalan.com/wp-content/uploads/2019/01/00004-Grossmont-District-Showcase-2018-D7A_5349.jpg | https://gcparksandresorts.github.io/gcresorthighland/destinations/adventure-park/entertainment/gc-marching-band \
-                        || Character Greetings | Daily | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/banners/banner5.jpg | https://gcparksandresorts.github.io/gcresorthighland/attractions";
-entertainmentList[2] = "one | two | three || four | five | six";
-entertainmentList[3] = "";
+
+// Standard Entertainment
+entertainmentList[1] = "Globe-City Marching Band | 12:00PM, 2:00PM, 3:00PM, 4:00PM | https://brettalan.com/wp-content/uploads/2019/01/00004-Grossmont-District-Showcase-2018-D7A_5349.jpg | https://gcparksandresorts.github.io/gcresorthighland/destinations/adventure-park/entertainment/gc-marching-band";
+entertainmentList[2] = "Character Greetings | Daily | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/banners/banner5.jpg | https://gcparksandresorts.github.io/gcresorthighland/attractions";
+entertainmentList[3] = "Drummers of the Mandahari | 12:30PM, 1:30PM, 3:30PM, 5:00PM, 6:00PM | https://drummingreview.com/wp-content/uploads/2023/02/History-of-Taiko-Drumming.webp | https://gcparksandresorts.github.io/gcresorthighland/attractions";
 entertainmentList[4] = "";
+
+// SummerFest Entertainment
+entertainmentList[5] = "Summers to Remember Forever | 9:30PM | https://www.tripsavvy.com/thmb/QUGooCRqxcg7uCB_oIVdZpaMumU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/12_2018_DL.2342-a4227ae6881c4341a1f1528aafa3360c.jpg | https://gcparksandresorts.github.io/gcresorthighland/events/summerfest";
+entertainmentList[6] = "La Musicas De Mariachi | 12:00PM, 1:00PM, 2:00PM, 5:00PM | https://www.alleytheatre.org/app/uploads/2023/07/Mariachi-Day-1200x834.jpg.webp | https://gcparksandresorts.github.io/gcresorthighland/events/summerfest";
+entertainmentList[7] = "SummerFest Boulevard Blast | 8:30PM | https://ca-times.brightspotcdn.com/dims4/default/45c1555/2147483647/strip/true/crop/1943x2473+0+0/resize/1200x1527!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F04%2Fb4%2Ffb9962a343e08aedcb38689444f4%2Fmsep-encantofloat.jpg | https://gcparksandresorts.github.io/gcresorthighland/events/summerfest"; 
+
+// Halloween Entertainment
+entertainmentList[8] = "Villanous Dance Party | 8:30PM, 11:00PM | Image_link | click_link";
+entertainmentList[9] = "Sally's Spooktacular Fireworks | 10:00PM | Image_link | click_link";
+entertainmentList[10] = "Name | Hours | Image_link | click_link";
+
+// Holiday Entertainment
+entertainmentList[11] = "Spirit of the Holidays Spectacular | 9:30PM | Image_link | click_link";
+entertainmentList[12] = "Dance of the Winter Fairies | 12:00PM, 1:00PM, 2:00PM, 4:00PM, 5:00PM | Image_link | click_link";
+entertainmentList[13] = "Season of Snow Cavalcade | 6:00PM, 8:00PM | Image_link | click_link";
+
+// Special Entertainiment
+entertainmentList[14] = "Fourth of July Fireworks | 9:30 PM | Image_link | click_link";
+entertainmentList[15] = "Founder's Day Fireworks | 8:30 PM | Image_link | click_link";
+
 
 
 // ------------------------------------------------------------------------------ SPECIAL EVENTS ------------------------------------------------------------------------------------------------
 eventList = []; // Name | Hours (or 'special event') | Image_Link | Click_Link ||  Name | Hours (or 'special event') | Image_Link | Click_Link || ...
+// eventList[N] = "name | hours | image_link | click_link";
 
 eventList[0] = "No Events Today";
 eventList[1] = "RMember Admission Discounts | Promotion | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/banners/banner4.jpg | www.google.com";
-eventList[2] = "Halloween at the Globe-City Resort Highland | Seasonal Event | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/banners/halloween02.png | https://gcparksandresorts.github.io/gcresorthighland/blog/posts/seasonal/halloween-at-the-resort ||\
-                Passholder Preview Day | Special Event | https://www.universalorlando.com/webdata/k2/en/us/files/Images/gds/uor-universal-annual-passholder-friends-shopping-a.jpg | https://gcparksandresorts.github.io/gcresorthighland/blog/posts/seasonal/halloween-at-the-resort";
-eventList[3] = "Halloween at the Globe-City Resort Highland | Seasonal Event | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/banners/halloween02.png | https://gcparksandresorts.github.io/gcresorthighland/blog/events/halloween/9-9-23-halloween-at-the-globe-city-resort.html";
-eventList[4] = "No Events Today";
+eventList[3] = "Passholder Preview Day | Special Event | https://www.universalorlando.com/webdata/k2/en/us/files/Images/gds/uor-universal-annual-passholder-friends-shopping-a.jpg | https://gcparksandresorts.github.io/gcresorthighland/blog/posts/seasonal/halloween-at-the-resort";
+eventList[4] = "Globe-City Celebrates Pride | Special Event | https://insidethemagic.net/wp-content/uploads/2023/04/Pride-Nite-1.jpg | https://gcparksandresorts.github.io/gcresorthighland/blog/posts/seasonal/pride";
+eventList[5] = "Adventurous Nights of Pride | Special Event | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/logos/nop.jpg | https://gcparksandresorts.github.io/gcresorthighland/events/nights-of-pride";
+
+eventList[6] = "SummerFest Celebration | Special Event | https://cpfoodblog.com/wp-content/uploads/2019/06/2019-Knotts-Summer-Nights-Smores.jpg | https://gcparksandresorts.github.io/gcresorthighland/events/summerfest";
+eventList[7] = "Fourth of July Celebration | Special Event | image_link | click_link";
+
+
+eventList[6] = "Halloween at the Globe-City Resort Highland | Seasonal Event | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/banners/halloween02.png | https://gcparksandresorts.github.io/gcresorthighland/blog/posts/seasonal/halloween-at-the-resort";
+eventList[7] = "Sally's Halloween Party | 7:00 PM to 12:00 AM | https://www.azcentral.com/gcdn/presto/2019/04/17/PPHX/2d57cc62-07ed-4659-8bc6-7f18cebc7a1b-Oogie_Boogie_Bash_Hero_Image.jpg?crop=2699,1518,x1,y207&width=1600&height=800&format=pjpg&auto=webp | https://gcparksandresorts.github.io/gcresorthighland/events/sallys-halloween-party";
+
+eventList[8] = "name | hours | image_link | click_link";
+eventList[9] = "name | hours | image_link | click_link";
 
 
 dateTierList = [];
