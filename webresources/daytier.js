d8585bb693dab695071b9b8dee9bf63011ff17b9
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-09-11 18:55:31 -0700

    Update daytier.js

--- webresources/daytier.js
+++ webresources/daytier.js
@@ -59,7 +59,7 @@ eventList[6] = "SummerFest Celebration | Special Event | https://cpfoodblog.com/
 eventList[7] = "Fourth of July Celebration | Special Event | image_link | click_link";
 
 
-eventList[8] = "Halloween at the Globe-City Resort Highland | Seasonal Event | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/banners/halloween02.png | https://gcparksandresorts.github.io/gcresorthighland/blog/posts/seasonal/halloween-at-the-resort";
+eventList[8] = "Halloween at the Globe-City Resort Highland | Seasonal Event | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/banners/halloween02.png | https://gcparksandresorts.github.io/gcresorthighland/blog/events/halloween/9-9-23-halloween-at-the-globe-city-resort.html";
 eventList[9] = "Sally's Halloween Party | 7:00 PM to 12:00 AM | https://www.azcentral.com/gcdn/presto/2019/04/17/PPHX/2d57cc62-07ed-4659-8bc6-7f18cebc7a1b-Oogie_Boogie_Bash_Hero_Image.jpg?crop=2699,1518,x1,y207&width=1600&height=800&format=pjpg&auto=webp | https://gcparksandresorts.github.io/gcresorthighland/events/sallys-halloween-party";
 
 eventList[10] = "name | hours | image_link | click_link";
