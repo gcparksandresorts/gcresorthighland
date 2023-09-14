5cefec8d778308e78373c9d4e85a9a617e741572
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-09-14 12:39:33 -0700

    Create table-of-contents.js

---
+++ blog/table-of-contents.js
@@ -0,0 +1,14 @@
+// This is the file which controls what shows on the blog homepage.
+// The first item on the list will show last, and the last item on the list will show first.
+
+// Category Options: Attractions | Entertainment | Dining | Merchandise | Events | Operations | Admission | Community | Other
+
+// Formatting: category | blog_title | link_to_blog -> 
+//  toc[365] = "category | blog_title | blog/link_to_blog";
+
+toc = [];
+
+  toc[3] = "Community | Helping the Highland Community | blog/community/9-14-24-helping-highland";
+  toc[2] = "Events | Dia de Los Muertos at Plaza del Mexico | blog/events/halloween/9-10-23-dia-de-los-muertos";
+  toc[1] = "Events | Halloween At the Globe-City Resort | blog/events/halloween/9-9-23-halloween-at-the-globe-city-resort";
+  toc[0] = "Operations | New Resort Blog | blog/operations/9-14-21-new-resort-blog";
