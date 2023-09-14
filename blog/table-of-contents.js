031b4b60b3b6e76e5286ece18611a9932caeb085
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-09-14 12:49:21 -0700

    Update table-of-contents.js

--- blog/table-of-contents.js
+++ blog/table-of-contents.js
@@ -3,12 +3,12 @@
 
 // Category Options: Attractions | Entertainment | Dining | Merchandise | Events | Operations | Admission | Community | Other
 
-// Formatting: category | blog_title | link_to_blog -> 
-//  toc[365] = "category | blog_title | blog/link_to_blog";
+// Formatting: category | blog_title | Date of Posting | link_to_blog -> 
+//  toc[365] = "category | blog_title | date_of_posting | blog/link_to_blog";
 
 toc = [];
 
-  toc[3] = "Community | Helping the Highland Community | blog/community/9-14-24-helping-highland";
-  toc[2] = "Events | Dia de Los Muertos at Plaza del Mexico | blog/events/halloween/9-10-23-dia-de-los-muertos";
-  toc[1] = "Events | Halloween At the Globe-City Resort | blog/events/halloween/9-9-23-halloween-at-the-globe-city-resort";
-  toc[0] = "Operations | New Resort Blog | blog/operations/9-14-21-new-resort-blog";
+  toc[3] = "Community | Helping the Highland Community | September 14, 2023 | blog/community/9-14-24-helping-highland";
+  toc[2] = "Events | Dia de Los Muertos at Plaza del Mexico | September 12, 2023 | blog/events/halloween/9-10-23-dia-de-los-muertos";
+  toc[1] = "Events | Halloween At the Globe-City Resort | September 9, 2023 | blog/events/halloween/9-9-23-halloween-at-the-globe-city-resort";
+  toc[0] = "Operations | New Resort Blog | September 14, 2023 | blog/operations/9-14-21-new-resort-blog";
