fa1d15ac23d6bb69836a30e8f11bd93ec16adeba
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-09-18 09:37:42 -0700

    Update table-of-contents.js

--- blog/table-of-contents.js
+++ blog/table-of-contents.js
@@ -8,7 +8,8 @@
 
 toc = [];
 
-  toc[3] = "Community | Helping the Highland Community | September 14, 2023 | blog/community/9-14-24-helping-highland";
+  toc[4] = "Dining | Preview: The New and Improved Le Boutique Bistro | September 18, 2023 | blog/dining/9-18-23-le-boutique-bistro";
+  toc[3] = "Community | Preview: Helping the Highland Community | September 14, 2024 | blog/community/9-14-24-helping-highland";
   toc[2] = "Events | Dia de Los Muertos at Plaza del Mexico | September 12, 2023 | blog/events/halloween/9-10-23-dia-de-los-muertos";
   toc[1] = "Events | Halloween At the Globe-City Resort | September 9, 2023 | blog/events/halloween/9-9-23-halloween-at-the-globe-city-resort";
   toc[0] = "Operations | New Resort Blog | September 14, 2023 | blog/operations/9-14-21-new-resort-blog";
