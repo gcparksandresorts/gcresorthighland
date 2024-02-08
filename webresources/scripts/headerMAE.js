c2066ac60ef7b9eb34ab78d1645be3e00f66a7e6
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-02-08 15:42:12 -0800

    Update headerMAE.js

--- webresources/scripts/headerMAE.js
+++ webresources/scripts/headerMAE.js
@@ -11,7 +11,7 @@ function runHeaderSetup(){
 function initializeHeader(){
 		console.log('resizing header'); 
     	Header.innerHTML = "<div id='headerSpacer'></div> \
-                        <div id='headerBar'><img src='https://gcparksandresorts.github.io/gcresorthighland/webresources/images/logos/gcResortLogoRemade.png'><br> \
+                        <div id='headerBar'><img src='https://gcparksandresorts.github.io/gcresorthighland/webresources/images/logos/gcResortLogoRemade.png' onclick='goHome()'><br> \
                         <a href='https://gcparksandresorts.github.io/gcresorthighland/meetings-and-events'>Home</a> \
                         <a href='https://gcparksandresorts.github.io/gcresorthighland/meetings-and-events/meeting-spaces'>Meeting Spaces</a> \
                         <a>Event Planning</a> \
@@ -21,3 +21,7 @@ function initializeHeader(){
 	document.getElementById('loader').style.display = 'none';
 	
 }//end of function initializeHeader
+
+function goHome(){
+	window.open("https://gcparksandresorts.github.io/gcresorthighland","_self");
+}
