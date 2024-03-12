405ecbfda786f0813fdd7415ca706cba7dc8fdeb
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-03-12 09:02:17 -0700

    Update daytier.js

--- webresources/daytier.js
+++ webresources/daytier.js
@@ -202,21 +202,21 @@ dateTierList[71] = "March 12 - Tuesday - 2 - 1,2,3 - 1,18";
 dateTierList[72] = "March 13 - Wednesday - 1 - 1,2,4 - 1,18";
 dateTierList[73] = "March 14 - Thursday - 1 - 1,2,3 - 1,18";
 dateTierList[74] = "March 15 - Friday - 3 - 1,2,3,4 - 1,18";
-dateTierList[75] = "March 16 - Saturday - 5 - 1,2,3,4 - 1,18,23"; //SN //COMMUNITY COLLEGE SPRING BREAK START
+dateTierList[75] = "March 16 - Saturday - 4 - 1,2,3,4 - 1,18,23"; //SN //COMMUNITY COLLEGE SPRING BREAK START
 dateTierList[76] = "March 17 - Sunday - 2 - 1,2,3,4 - 1,18";
 dateTierList[77] = "March 18 - Monday - 2 - 1,2,3,4 - 1,18";
 dateTierList[78] = "March 19 - Tuesday - 2 - 1,2,3 - 1,18";
 dateTierList[79] = "March 20 - Wednesday - 2 - 1,2,3 - 1,18";
 dateTierList[80] = "March 21 - Thursday - 2 - 1,2,4 - 1,18";
 dateTierList[81] = "March 22 - Friday - 3 - 1,2,3,4 - 1,18";
-dateTierList[82] = "March 23 - Saturday - 5 - 1,2,3,4,6 - 1,18,23"; //SN ONUSD SPRING BREAK START
+dateTierList[82] = "March 23 - Saturday - 4 - 1,2,3,4,6 - 1,18,23"; //SN ONUSD SPRING BREAK START
 dateTierList[83] = "March 24 - Sunday - 4 - 1,2,3,4,6 - 1,18"; 
 dateTierList[84] = "March 25 - Monday - 3 - 1,2,3 - 1,18";
 dateTierList[85] = "March 26 - Tuesday - 4 - 1,2,3,6 - 1,18";
 dateTierList[86] = "March 27 - Wednesday - 3 - 1,2,3,6 - 1,18";
-dateTierList[87] = "March 28 - Thursday - 4 - 1,2,3,4,6 - 1,18";
-dateTierList[88] = "March 29 - Friday - 5 - 1,2,3,4,6 - 1,18";
-dateTierList[89] = "March 30 - Saturday - 5 - 1,2,3,4,6 - 1,18,23";// Last Saturday Nightz
+dateTierList[87] = "March 28 - Thursday - 3 - 1,2,3,4,6 - 1,18";
+dateTierList[88] = "March 29 - Friday - 4 - 1,2,3,4,6 - 1,18";
+dateTierList[89] = "March 30 - Saturday - 4 - 1,2,3,4,6 - 1,18,23";// Last Saturday Nightz
 dateTierList[90] = "March 31 - Sunday - 4 - 1,2,3,4 - 1,18"; //ONUSD SPRING BREAK END
 
 
